b12f9e8f7cd5a74a8ffb06186cfbdbe0b10e6fd1
gengzhiguo <gengzhiguo@xiongmaojinfu.com>
2017-11-06 14:55:39 +0800

    basic 代码

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h3>Increment:</h3>
+    <increment></increment>
+    <h3>Counter:</h3>
+    <counter></counter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import Counter from './components/CounterDisplay.vue'
+import Increment from './components/IncrementButton.vue'
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Counter,
+    Increment
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Count is {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
