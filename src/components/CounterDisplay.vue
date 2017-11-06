a74ad47d9ff6f47be4d65f454b934b5dc9b70f42
gengzhiguo <gengzhiguo@xiongmaojinfu.com>
2017-11-06 15:07:56 +0800

    solution 1 code

---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Count is {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  events: {
+    increment () {
+      this.count ++
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click.prevent="activate">+1</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    activate () {
+      // Send an event upwards to be picked up by App
+      this.$dispatch('button-pressed')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
